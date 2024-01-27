e0ab03cbf4ea3305ea2bbaf67eeafe95e1bb5c0d
Your Name <you@example.com>
2024-01-27 03:28:04 +0100

    ..

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
+
 class Kernel extends ConsoleKernel
 {
     /**
