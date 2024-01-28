e7b2bfe515bbcda41898b62046976e58d2355c68
Your Name <you@example.com>
2024-01-28 01:17:42 +0100

    scheduler

--- routes/api.php
+++ routes/api.php
@@ -27,13 +27,13 @@ Route::middleware(['basicAuth'])->group(function () {
         });
     });
 
+});
 
-    Route::prefix('v1')->group(function () {
-        Route::prefix('secretcontroller')->group(function () {
-            Route::controller(\App\Http\Controllers\V1\SecretController::class)->group(function () {
-                Route::get('scheduler', 'scheduler');
-            });
+Route::prefix('v1')->group(function () {
+    Route::prefix('secretcontroller')->group(function () {
+        Route::controller(\App\Http\Controllers\V1\SecretController::class)->group(function () {
+            Route::get('scheduler', 'scheduler');
         });
     });
-
 });
+
