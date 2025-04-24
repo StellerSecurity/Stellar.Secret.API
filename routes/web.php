0661b84ee89c4882a11b6c7c78b5d43c14272983
Bob <150491881+stellar-security-os@users.noreply.github.com>
2025-04-24 18:18:34 +0200

    Added stop-when-empty command.

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    //Artisan::call('queue:work');
+    Artisan::call('queue:work --stop-when-empty');
     return "";
 });
