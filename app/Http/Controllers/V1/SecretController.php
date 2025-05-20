65de49e9958d2932115ea44a51b1ab1e495eb11c
kalle <info@stellarsecurity.com>
2025-05-21 01:49:44 +0200

    Added File-deletion if user burns their message.

--- app/Http/Controllers/V1/SecretController.php
+++ app/Http/Controllers/V1/SecretController.php
@@ -86,12 +86,12 @@ class SecretController extends Controller
             return response()->json(['response_code' => Response::HTTP_BAD_REQUEST]);
         }
 
-        $secret->delete();
-
         $file = $this->externalStorageService->file($secret->id);
         if($file->status() !== null && $file->status() == Response::HTTP_OK) {
             SecretFileUploadHelper::deleteIfFailedTryAgain($secret->id);
         }
+
+        $secret->delete();
 
         //FileUpload::where('secret_id', $request->input('secret_id'))->delete();
 
